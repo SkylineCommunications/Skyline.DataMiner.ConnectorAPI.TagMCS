aa52c4ccd6eceb9d7280aca2bd5bc660a62e4d2e
SKYLINE2\CarlosMT <carlos.matheus@skyline.be>
2025-11-12 12:20:18 -0500

    Add request and response cs for UpdateChannel

---
+++ ConnectorAPI.TagMCS/InterApp/Messages/UpdateChannelRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.TAGVideoSystems.MCS.InterApp.Messages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
+
+    public class UpdateChannelRequest : Message
+    {
+        /// <summary>
+        /// The ID of the Channel
+        /// </summary>
+        public string Channel { get; set; }
+
+
+        /// <summary>
+        /// Constructor for the Get Channel Configuration Request
+        /// </summary>
+        /// <param name="channel"></param>
+        public UpdateChannelRequest(string channel)
+        {
+            Channel = channel;
+        }
+    }
+}
