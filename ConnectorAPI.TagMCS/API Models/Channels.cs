92a1546b331d19beb27d8f74e686a4b0826ff3e6
Gabriel Borges <gabriel.borges@skyline.be>
2025-11-20 15:31:31 -0500

    Update Channels.cs

--- ConnectorAPI.TagMCS/API Models/Channels.cs
+++ ConnectorAPI.TagMCS/API Models/Channels.cs
@@ -231,7 +231,7 @@ namespace Skyline.DataMiner.ConnectorAPI.TAGVideoSystems.MCS.API_Models
 		public bool Enabled { get; set; }
 
 		[JsonProperty("program_number")]
-		public string ProgramNumber { get; set; }
+		public int ProgramNumber { get; set; }
 
 		[JsonProperty("attributes")]
 		public ReceiverAttributes Attributes { get; set; }
