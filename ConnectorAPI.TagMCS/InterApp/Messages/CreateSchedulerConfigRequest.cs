bb74924d600b496a817f9a054a20415eafe8552c
GeovannyGA <geovanny.galeano@skyline.be>
2025-11-24 14:30:04 -0500

    Addition of create scheduler config request.

---
+++ ConnectorAPI.TagMCS/InterApp/Messages/CreateSchedulerConfigRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.TAGVideoSystems.MCS.InterApp.Messages
+{
+    using Skyline.DataMiner.ConnectorAPI.TAGVideoSystems.MCS.API_Models;
+    using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
+
+    /// <summary>
+    /// Inter-app call request message to create a new scheduler configuration.
+    /// </summary>
+    /// <remarks>
+    /// Populate <see cref="SchedulerConfig"/> with the desired scheduler configuration details before sending this message.
+    /// </remarks>
+    public class CreateSchedulerConfigRequest : Message
+    {
+        /// <summary>
+        /// Gets or sets the scheduler configuration to be created.
+        /// </summary>
+        public SchedulerConfig SchedulerConfig { get; set; }
+    }
+}
