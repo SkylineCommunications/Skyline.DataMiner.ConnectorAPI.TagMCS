0ff64602f60433499a47299b37b4c265e3f3f829
Gabriel Borges <gabriel.borges@skyline.be>
2025-12-02 13:31:43 -0500

    Create SetPenaltyBoxConfigRequest.cs

---
+++ ConnectorAPI.TagMCS/InterApp/Messages/SetPenaltyBoxConfigRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.TAGVideoSystems.MCS.InterApp.Messages
+{
+    using Skyline.DataMiner.ConnectorAPI.TAGVideoSystems.MCS.API_Models;
+    using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
+
+    /// <summary>
+    /// Request to update a Layout Configuration on TAG
+    /// </summary>
+    public class SetPenaltyBoxConfigRequest : Message
+    {
+        /// <summary>
+        /// The Penalty Box Configuration to be set on TAG
+        /// </summary>
+        public PenaltyBoxData PenaltyBox { get; set; }
+    }
+}
